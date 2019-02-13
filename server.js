8ccb581c932f0b0a17c19f9cbbecaf605e9c1415
Felix Crazzolara <felix.crazzolara@gmail.com>
2019-02-13 21:54:04 +0100

    Added code to connect to Ganache using web3.js. Does not work. Returns with same error message as described here: https://github.com/ethereum/web3.js/issues/1916#issuecomment-427398031

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Http server
-|--------------------------------------------------------------------------
-|
-| This file bootstrap Adonisjs to start the HTTP server. You are free to
-| customize the process of booting the http server.
-|
-| """ Loading ace commands """
-|     At times you may want to load ace commands when starting the HTTP server.
-|     Same can be done by chaining `loadCommands()` method after
-|
-| """ Preloading files """
-|     Also you can preload files by calling `preLoad('path/to/file')` method.
-|     Make sure to pass relative path from the project root.
-*/
+// Import fs
+const fs = require('fs');
 
-const { Ignitor } = require('@adonisjs/ignitor')
+// Contract parameter
+const contractAddress = '0xcfDb7F01f13AD31e9F1738DCEAb4f063251048D7';
+const json = JSON.parse(fs.readFileSync('./SmartContract.json', 'utf8'));
+const nodeAddress = '0xF1CEE9e340b874Fabb54Ab2F01125e9396884f26';
 
-new Ignitor(require('@adonisjs/fold'))
-  .appRoot(__dirname)
-  .fireHttpServer()
-  .catch(console.error)
+// Import libraries
+const Web3     = require('web3');
+// const Personal = require('web3-eth-personal');
+// const personal = new Personal(new Web3.providers.HttpProvider('http://localhost:7545'));
+const provider = new Web3.providers.HttpProvider('http://localhost:7545');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+const Contract = require('web3-eth-contract');
+Contract.setProvider(provider);
+// const contract = new Contract(json.abi, contractAddress, {from: nodeAddress, gasPrice: '20000000000', gas: 400000});
+const contract = new Contract(json.abi, contractAddress, {from: nodeAddress});
+
+// contract.methods.get()
+// .call()
+// .then(function(receipt) {
+//     console.log(receipt);
+// })
+// .catch(function(error) {
+//     console.log(error);
+// });
+
+contract.methods.add(3,4)
+.call()
+.then(function(result) {
+    console.log(result);
+})
+.catch(function(error) {
+    console.log(error);
+});
+
